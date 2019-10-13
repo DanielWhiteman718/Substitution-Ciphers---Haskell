a9cf4a23c8b550d74b1e91484233d4cf8053c1b2
Daniel Whiteman <danielwhiteman718@gmail.com>
2019-10-13 20:24:15 +0100

    validateCipher function

--- Ciphers/CipherAssignment1.hs
+++ Ciphers/CipherAssignment1.hs
@@ -1,2 +1,16 @@
-Module Ciphers where
- import AssignmentHelp
+import AssignmentHelp
+import Data.List
+
+oddx :: Int -> Bool
+oddx n = n `mod` 2 /= 0
+
+oddItems :: [Int] -> [Int]
+oddItems [] = []
+oddItems numbers = [n | n <- numbers, oddx n]
+
+
+alphabet :: String
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+validateCipher :: Cipher -> Bool
+validateCipher cipher = sort cipher == alphabet
