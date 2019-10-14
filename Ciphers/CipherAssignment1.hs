b53f99f0d1f5aa68cf5ff11b4ec543e4615430ab
Daniel Whiteman <danielwhiteman718@gmail.com>
2019-10-14 14:32:58 +0100

    encode message start

--- Ciphers/CipherAssignment1.hs
+++ Ciphers/CipherAssignment1.hs
@@ -4,6 +4,11 @@ import Data.List
 oddx :: Int -> Bool
 oddx n = n `mod` 2 /= 0
 
+oddItems :: [Int] -> [Int]
+oddItems [] = []
+oddItems numbers = [n | n <- numbers, oddx n]
+
+
 alphabet :: String
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -12,3 +17,8 @@ validateCipher cipher = sort cipher == alphabet
 
 encode :: Cipher -> Int -> Char -> Char
 encode cipher offset char = cipher !! (((alphaPos char) + offset) `mod` 26)
+
+encodeMessage :: Cipher -> Int -> String -> [Char]
+encodeMessage cipher offset message = map f 
+                                      where f x = encode cipher offset x
+
