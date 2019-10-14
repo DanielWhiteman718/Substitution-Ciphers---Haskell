660b9fc6523fb50a8e16a591e948652f5fe4768e
Daniel Whiteman <danielwhiteman718@gmail.com>
2019-10-14 13:33:17 +0100

    Getting rid of homework code

--- Ciphers/CipherAssignment1.hs
+++ Ciphers/CipherAssignment1.hs
@@ -4,11 +4,6 @@ import Data.List
 oddx :: Int -> Bool
 oddx n = n `mod` 2 /= 0
 
-oddItems :: [Int] -> [Int]
-oddItems [] = []
-oddItems numbers = [n | n <- numbers, oddx n]
-
-
 alphabet :: String
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
