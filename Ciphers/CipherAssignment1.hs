80468867ef03001ce92bc16508e4e7dccfbd8b88
Daniel Whiteman <danielwhiteman718@gmail.com>
2019-10-15 16:28:42 +0100

    reverseEncode

--- Ciphers/CipherAssignment1.hs
+++ Ciphers/CipherAssignment1.hs
@@ -1,13 +1,6 @@
 import AssignmentHelp
 import Data.List
-
-oddx :: Int -> Bool
-oddx n = n `mod` 2 /= 0
-
-oddItems :: [Int] -> [Int]
-oddItems [] = []
-oddItems numbers = [n | n <- numbers, oddx n]
-
+--import Data.Maybe
 
 alphabet :: String
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,9 +11,8 @@ validateCipher cipher = sort cipher == alphabet
 encode :: Cipher -> Int -> Char -> Char
 encode cipher offset char = cipher !! (((alphaPos char) + offset) `mod` 26)
 
-
-
-
 encodeMessage :: Cipher -> Int -> String -> String
 encodeMessage cipher offset message = [encode cipher offset n | n <- message]
 
+reverseEncode :: Cipher -> Int -> Char -> Char
+reverseEncode cipher offset char = alphabet !! ((fromMaybe (elemIndex char cipher)) - offset)
