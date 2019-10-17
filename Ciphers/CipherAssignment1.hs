6642525f103fe6935e83394e94656c2fa3ae4743
Daniel Whiteman <danielwhiteman718@gmail.com>
2019-10-17 23:04:29 +0100

    LetterStats function

--- Ciphers/CipherAssignment1.hs
+++ Ciphers/CipherAssignment1.hs
@@ -19,3 +19,22 @@ reverseEncode cipher offset char = alphabet !! ((fromMaybe (elemIndex char ciphe
 
 reverseEncodeMessage :: Cipher -> Int -> String -> String
 reverseEncodeMessage cipher offset message = [reverseEncode cipher offset n | n <- message] 
+
+removeDuplicate :: String -> String
+removeDuplicate [] = []
+removeDuplicate [a] = [a]
+removeDuplicate text = head text:(removeDuplicate (delete (head text) (tail text)))
+
+allInst :: Char -> String -> [Char]
+allInst letter message = [n | n <- message, n == letter] 
+
+letterPerc :: String -> String -> Int
+letterPerc message instances = quot ((length instances)*100) (length message)
+
+statTuple :: Char -> Int -> (Char, Int)
+statTuple letter percentage = (letter, percentage)
+
+letterStats :: String -> [(Char, Int)]
+letterStats message = sortBy (\(_,a) (_,b) -> compare a b) [statTuple n (letterPerc message (allInst n message)) 
+                                                                            | n <- removeDuplicate message]
+
